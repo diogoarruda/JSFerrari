55722de3204f23b0ea9b2772a5663652429d7519
Diogo Arruda <diogo.ra@hotmail.com>
2021-01-21 17:01:16 -0300

    first commit

---
+++ public/assets/scripts/index.js
@@ -0,0 +1,5 @@
+import './main'
+import './auth'
+import './calendar'
+import './time-options'
+import './services'
---
+++ public/assets/scripts/time-options.js
@@ -0,0 +1,96 @@
+import { format, parse } from "date-fns"
+import { ptBR } from 'date-fns/locale'
+import firebase from './firebase-app'
+import { appendTemplate, getFormValues, getQueryString, setFormValues } from "./utils"
+
+const renderTimeOptions = (context, timeOptions) => {
+
+    const targetElement = context.querySelector(".options")
+
+    targetElement.innerHTML = ""
+
+    timeOptions.forEach(item => {
+
+        appendTemplate(
+            targetElement,
+            "label",
+            `
+                <input type="radio" name="option" value="${item.value}" />
+                <span>${item.value}</span>
+            `
+        )
+
+    })
+
+    
+
+}
+
+const validateSubmitForm = context => {
+
+    const button = context.querySelector("[type=submit]")
+
+    const checkValue = () => {
+
+        if (context.querySelector("[name=option]:checked")) {
+            button.disabled = false
+        } else {
+            button.disabled = true
+        }
+
+    }
+
+    window.addEventListener('load', e => checkValue())
+
+    context.querySelectorAll("[name=option]").forEach(input => {
+
+        input.addEventListener("change", e => {
+
+            //button.disabled = !context.querySelector("[name=option]:checked")
+            checkValue()            
+
+        })
+
+    })
+
+    context.querySelector("form").addEventListener("submit", e => {
+
+        if (!context.querySelector("[name=option]:checked")) {
+            button.disabled = true
+            e.preventDefault()
+        }
+
+    })
+
+}
+
+document.querySelectorAll("#time-options").forEach(page => {
+
+    const db = firebase.firestore();
+
+    db.collection('time-options').onSnapshot(snapshot => {
+
+        const timeOptions = [];
+        
+        snapshot.forEach(item => {
+
+            timeOptions.push(item.data());
+
+        })
+
+        renderTimeOptions(page, timeOptions);
+
+        validateSubmitForm(page)
+
+    });
+
+    const params = getQueryString()
+    const title = page.querySelector("h3")
+    const form = page.querySelector("form")
+    const scheduleAt = parse(params.schedule_at, "yyyy-MM-dd", new Date())
+
+    setFormValues(form, params)
+
+    title.innerHTML = format(scheduleAt, "EEEE, d 'de' MMMM 'de' yyyy", { locale: ptBR})
+
+})
